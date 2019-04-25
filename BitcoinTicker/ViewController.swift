69cf60ea50dfefaa9f54137f3565da99c4e4213c
diptendulkar <diptendu.tu@gmail.com>
2019-04-25 15:09:15 +0530

    added pod files and API call with bitcoin value updated

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -5,6 +5,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource{
 
@@ -12,6 +14,7 @@ class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataS
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbollArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
     var finalURL = ""
 
     //Pre-setup IBOutlets
@@ -28,6 +31,11 @@ class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataS
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        getCoinData(url : finalURL,CurrencyPos : row)
     }
     
     override func viewDidLoad() {
@@ -50,46 +58,47 @@ class ViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataS
 //    
 //    //MARK: - Networking
 //    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
+    
+    func getCoinData(url: String, CurrencyPos : Int) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Sucess! Got the Coin data")
+                    let coinJSON : JSON = JSON(response.result.value!)
+
+                    print(coinJSON)
+                    self.updateCoinData(json: coinJSON,pos : CurrencyPos)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+
 //
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
 //    
 //    
 //    
 //    //MARK: - JSON Parsing
 //    /***************************************************************/
 //    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
+    func updateCoinData(json : JSON, pos : Int) {
+        
+        if let coinResult = json["ask"].double {
+        bitcoinPriceLabel.text = currencySymbollArray[pos] + String(coinResult)
 //        weatherData.temperature = Int(round(tempResult!) - 273.15)
 //        weatherData.city = json["name"].stringValue
 //        weatherData.condition = json["weather"][0]["id"].intValue
 //        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
+        }
+        
 //        updateUIWithWeatherData()
-//    }
-//    
+    }
+//
 
 
 
